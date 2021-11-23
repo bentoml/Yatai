993923368af00b1c315480e18a30fa41828ebd0d
Rishi Kothari <itsrishikothari@gmail.com>
2021-11-22 19:27:04 -0500

    ci(ci): :rotating_light: Update error capacity for no-console

--- dashboard/.eslintrc.js
+++ dashboard/.eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'error',
         '@typescript-eslint/no-unused-vars': 'error',
-        'no-console': "on",
+        'no-console': 'error',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/naming-convention': [
