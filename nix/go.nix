d496d94e480e76c4d7eba998637889e4a38e7f4d
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2022-05-10 12:48:11 -0400

    chore: lint & fmt with nixpkgs-fmt
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- nix/go.nix
+++ nix/go.nix
@@ -4,19 +4,19 @@ with pkgs; let
   toGoKernel = platform:
     if platform.isDarwin then "darwin"
     else platform.parsed.kernel.name;
-    hashes = {
-      # Use `./shells/print-hashes.sh ${version}` to generate the list below
-      # https://raw.githubusercontent.com/NixOS/nixpkgs/master/pkgs/development/compilers/go/print-hashes.sh
-      darwin-amd64 = "765c021e372a87ce0bc58d3670ab143008dae9305a79e9fa83440425529bb636";
-      darwin-arm64 = "ffe45ef267271b9681ca96ca9b0eb9b8598dd82f7bb95b27af3eef2461dc3d2c";
-      linux-386 = "982487a0264626950c635c5e185df68ecaadcca1361956207578d661a7b03bee";
-      linux-amd64 = "550f9845451c0c94be679faf116291e7807a8d78b43149f9506c1b15eb89008c";
-      linux-arm64 = "06f505c8d27203f78706ad04e47050b49092f1b06dc9ac4fbee4f0e4d015c8d4";
-      linux-armv6l = "aa0d5516c8bd61654990916274d27491cfa229d322475502b247a8dc885adec5";
-      linux-ppc64le = "b821ff58d088c61adc5d7376179a342f325d8715a06abdeb6974f6450663ee60";
-      linux-s390x = "7d1727e08fef295f48aed2b8124a07e3752e77aea747fcc7aeb8892b8e2f2ad2";
+  hashes = {
+    # Use `./shells/print-hashes.sh ${version}` to generate the list below
+    # https://raw.githubusercontent.com/NixOS/nixpkgs/master/pkgs/development/compilers/go/print-hashes.sh
+    darwin-amd64 = "765c021e372a87ce0bc58d3670ab143008dae9305a79e9fa83440425529bb636";
+    darwin-arm64 = "ffe45ef267271b9681ca96ca9b0eb9b8598dd82f7bb95b27af3eef2461dc3d2c";
+    linux-386 = "982487a0264626950c635c5e185df68ecaadcca1361956207578d661a7b03bee";
+    linux-amd64 = "550f9845451c0c94be679faf116291e7807a8d78b43149f9506c1b15eb89008c";
+    linux-arm64 = "06f505c8d27203f78706ad04e47050b49092f1b06dc9ac4fbee4f0e4d015c8d4";
+    linux-armv6l = "aa0d5516c8bd61654990916274d27491cfa229d322475502b247a8dc885adec5";
+    linux-ppc64le = "b821ff58d088c61adc5d7376179a342f325d8715a06abdeb6974f6450663ee60";
+    linux-s390x = "7d1727e08fef295f48aed2b8124a07e3752e77aea747fcc7aeb8892b8e2f2ad2";
 
-    };
+  };
 
   toGoCPU = platform: {
     "i686" = "386";
--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { sources ? import nix/sources.nix
- , pkgs ? import sources.nixpkgs { overlays = [] ; config = {}; }
+, pkgs ? import sources.nixpkgs { overlays = [ ]; config = { }; }
 }:
 
 with pkgs;
@@ -8,7 +8,7 @@ let
   inherit (lib) optional optionals;
 
   # custom defined packages in nixpkgs
-  go = callPackage ./nix/go.nix { pkgs=pkgs; };
+  go = callPackage ./nix/go.nix { pkgs = pkgs; };
   nodejs = nodejs-14_x;
   postgresql = postgresql_14;
 
@@ -40,9 +40,9 @@ let
   requiredPackages = basePackages
     ++ lib.optional stdenv.isLinux inotify-tools
     ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
-        CoreFoundation
-        CoreServices
-      ]);
+    CoreFoundation
+    CoreServices
+  ]);
 
   env = buildEnv {
     name = "build-env";
@@ -50,49 +50,49 @@ let
   };
 
 in
-  stdenv.mkDerivation rec {
-    name = "yatai-dev";
+stdenv.mkDerivation rec {
+  name = "yatai-dev";
 
-    phases = ["nobuild"];
-    buildInputs = [env];
+  phases = [ "nobuild" ];
+  buildInputs = [ env ];
 
-    shellHook = ''
-        # "nix-shell --pure" resets LANG to POSIX, this breaks "make TAGS".
-        export LANG="en_US.UTF-8"
-        # /bin/ps cannot be found in the build environment.
-        export PATH="/bin:/usr/bin:/usr/local/bin:/usr/sbin:$PATH"
+  shellHook = ''
+    # "nix-shell --pure" resets LANG to POSIX, this breaks "make TAGS".
+    export LANG="en_US.UTF-8"
+    # /bin/ps cannot be found in the build environment.
+    export PATH="/bin:/usr/bin:/usr/local/bin:/usr/sbin:$PATH"
 
-        # setup for dashboard
-        alias scripts='jq ".scripts" dashboard/package.json'
+    # setup for dashboard
+    alias scripts='jq ".scripts" dashboard/package.json'
 
-        make fe-deps be-deps
+    make fe-deps be-deps
 
-        export PGDATA=${pg_root}
-        export SOCKET_DIRECTORIES="$PWD/sockets"
-        mkdir $SOCKET_DIRECTORIES
+    export PGDATA=${pg_root}
+    export SOCKET_DIRECTORIES="$PWD/sockets"
+    mkdir $SOCKET_DIRECTORIES
 
-        if [ ! -d "$PGDATA" ]; then
-          initdb --auth=trust --auth-host=trust >/dev/null
-          echo "unix_socket_directories = '$SOCKET_DIRECTORIES'" >> $PGDATA/postgresql.conf
-          createuser ${pg_user} --createdb -h ${pg_host}
-          createdb ${pg_db} -h ${pg_host} k-O ${pg_user}
-        fi
+    if [ ! -d "$PGDATA" ]; then
+      initdb --auth=trust --auth-host=trust >/dev/null
+      echo "unix_socket_directories = '$SOCKET_DIRECTORIES'" >> $PGDATA/postgresql.conf
+      createuser ${pg_user} --createdb -h ${pg_host}
+      createdb ${pg_db} -h ${pg_host} k-O ${pg_user}
+    fi
 
-        pg_ctl -l $PGDATA/logfile start
+    pg_ctl -l $PGDATA/logfile start
 
-        function end {
-          echo "Shutting down the database..."
-          pg_ctl stop
-          echo "Removing directories..."
-          rm -rf $SOCKET_DIRECTORIES
-        }
-        trap end EXIT
-    '';
-    enableParallelBuilding = true;
+    function end {
+      echo "Shutting down the database..."
+      pg_ctl stop
+      echo "Removing directories..."
+      rm -rf $SOCKET_DIRECTORIES
+    }
+    trap end EXIT
+  '';
+  enableParallelBuilding = true;
 
-    LOCALE_ARCHIVE = if stdenv.isLinux then "${glibcLocales}/lib/locale/locale-archive" else "";
+  LOCALE_ARCHIVE = if stdenv.isLinux then "${glibcLocales}/lib/locale/locale-archive" else "";
 
-    nobuild = ''
-      echo Do not run this derivation with nix-build, it can only be used with nix-shell
-    '';
+  nobuild = ''
+    echo Do not run this derivation with nix-build, it can only be used with nix-shell
+  '';
 }
