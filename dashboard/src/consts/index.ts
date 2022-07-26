6f8b2cf096efc6ea8a9430ce60ed920d51b4a77d
yetone <yetoneful@gmail.com>
2022-07-26 18:21:11 +0000

    refactor: remove yatai components UI

--- dashboard/src/consts/index.ts
+++ dashboard/src/consts/index.ts
@@ -1,13 +1,11 @@
-import { YataiComponentType } from '@/schemas/yatai_component'
 import { ResourceType } from '@/schemas/resource'
 import type { IconType } from 'react-icons/lib'
 import { GrOrganization, GrServerCluster, GrDeploy, GrUser } from 'react-icons/gr'
-import { AiOutlineDashboard, AiOutlineCodeSandbox } from 'react-icons/ai'
+import { AiOutlineCodeSandbox } from 'react-icons/ai'
 import { HiOutlineUserGroup, HiOutlineKey } from 'react-icons/hi'
 import { BiRevision, BiExtension } from 'react-icons/bi'
 import { GoPackage } from 'react-icons/go'
 import { VscFileBinary } from 'react-icons/vsc'
-import { RiMistFill } from 'react-icons/ri'
 import { GiAbstract006, GiAbstract045 } from 'react-icons/gi'
 
 export const headerHeight = 55
@@ -33,10 +31,4 @@ export const resourceIconMapping: Record<ResourceType, IconType> = {
     api_token: HiOutlineKey,
     bento_runner: GiAbstract045,
     bento_api_server: GiAbstract006,
-}
-
-export const yataiComponentIconMapping: Record<YataiComponentType, IconType> = {
-    deployment: GrDeploy,
-    logging: RiMistFill,
-    monitoring: AiOutlineDashboard,
 }
--- dashboard/src/pages/Cluster/YataiComponents.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import YataiListCard from '@/components/YataiComponentListCard'
-
-export default function ClusterDeployments() {
-    const { clusterName } = useParams<{ clusterName: string }>()
-
-    return <YataiListCard clusterName={clusterName} />
-}
