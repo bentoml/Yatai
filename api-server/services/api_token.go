30a46eae3940018ae72175fd41d06b4a31f10e22
yetone <yetoneful@gmail.com>
2022-08-16 17:10:58 +0000

    fix: get api token

--- api-server/services/api_token.go
+++ api-server/services/api_token.go
@@ -12,9 +12,9 @@ import (
 
 	"github.com/bentoml/yatai-common/config"
 	"github.com/bentoml/yatai-common/consts"
+	"github.com/bentoml/yatai-common/utils"
 	"github.com/bentoml/yatai-schemas/modelschemas"
 	"github.com/bentoml/yatai/api-server/models"
-	"github.com/bentoml/yatai/common/utils"
 )
 
 type apiTokenService struct{}
