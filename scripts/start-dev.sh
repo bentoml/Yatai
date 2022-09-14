a0636851819aaee44b5160fadf478407e3d0c394
yetone <yetoneful@gmail.com>
2022-09-14 15:01:45 +0800

    feat: add start-dev script (#341)

---
+++ scripts/start-dev.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# check if jq command exists
+if ! command -v jq &> /dev/null; then
+  # download jq from github by different arch
+  case "$(uname -m)" in
+    x86_64)
+      JQ_ARCH=jq-linux64
+      ;;
+    aarch64)
+      JQ_ARCH=jq-linux64
+      ;;
+    armv7l)
+      JQ_ARCH=jq-linux32
+      ;;
+    Darwin)
+      JQ_ARCH=jq-osx-amd64
+      ;;
+    *)
+      echo "Unsupported architecture $(uname -m)"
+      exit 1
+      ;;
+  esac
+  echo "📥 downloading jq from github"
+  curl -sL -o /tmp/yatai-jq "https://github.com/stedolan/jq/releases/download/jq-1.6/${JQ_ARCH}"
+  echo "✅ downloaded jq to /tmp/yatai-jq"
+  chmod +x /tmp/yatai-jq
+  jq=/tmp/yatai-jq
+else
+  jq=$(which jq)
+fi
+
+telepresence connect
+
+env $(kubectl -n yatai-system get secret env -o jsonpath='{.data}' | $jq 'to_entries|map("\(.key)=\(.value|@base64d)")|.[]' | xargs) make be-run
+
+telepresence leave yatai-yatai-system || true
+telepresence intercept yatai -n yatai-system -p 7777:http
