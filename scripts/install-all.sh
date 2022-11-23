d8940ce78370845e2ec931b6119c318b0cbeed68
yetone <yetoneful@gmail.com>
2022-11-23 20:24:47 +0000

    feat(scripts): install yatai-image-builder

--- scripts/install-all.sh
+++ scripts/install-all.sh
@@ -3,7 +3,8 @@
 set -e
 
 bash <(curl -s "https://raw.githubusercontent.com/bentoml/yatai/main/scripts/quick-install-yatai.sh")
-bash <(curl -s "https://raw.githubusercontent.com/bentoml/yatai-deployment/v1.0.x/scripts/quick-install-yatai-deployment.sh")
+bash <(curl -s "https://raw.githubusercontent.com/bentoml/yatai-image-builder/main/scripts/quick-install-yatai-image-builder.sh")
+bash <(curl -s "https://raw.githubusercontent.com/bentoml/yatai-deployment/main/scripts/quick-install-yatai-deployment.sh")
 kubectl -n yatai-deployment rollout restart deploy/yatai-deployment || true
 bash <(curl -s "https://raw.githubusercontent.com/bentoml/yatai/main/scripts/quick-setup-yatai-monitoring.sh")
 bash <(curl -s "https://raw.githubusercontent.com/bentoml/yatai/main/scripts/quick-setup-yatai-logging.sh")
