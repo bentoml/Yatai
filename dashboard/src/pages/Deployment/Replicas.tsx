68ff89a1df5bedb9c9b10c3322fcb244ce8acff0
yetone <yetoneful@gmail.com>
2022-12-01 10:00:54 +0000

    feat(dashboard): remove docker image builder pods in replicas page

--- dashboard/src/pages/Deployment/Replicas.tsx
+++ dashboard/src/pages/Deployment/Replicas.tsx
@@ -9,8 +9,6 @@ import useTranslation from '@/hooks/useTranslation'
 import KubePodEvents from '@/components/KubePodEvents'
 import { MdEventNote } from 'react-icons/md'
 import { useDeployment } from '@/hooks/useDeployment'
-import Pods from '@/components/Pods'
-import { GrDocker } from 'react-icons/gr'
 
 export default function DeploymentReplicas() {
     const { clusterName, kubeNamespace, deploymentName } =
@@ -33,15 +31,6 @@ export default function DeploymentReplicas() {
             <Card title={t('replicas')} titleIcon={VscServerProcess}>
                 <PodList deployment={deployment} loading={podsLoading} pods={pods ?? []} groupByRunner />
             </Card>
-            {deployment?.latest_revision?.targets[0] && (
-                <Card title={t('docker image builder pods')} titleIcon={GrDocker}>
-                    <Pods
-                        clusterName={clusterName}
-                        namespace='yatai-builders'
-                        selector={`yatai.ai/bento-repository=${deployment?.latest_revision?.targets[0]?.bento.repository.name},yatai.ai/bento=${deployment?.latest_revision?.targets[0]?.bento.version}`}
-                    />
-                </Card>
-            )}
             <Card title={t('events')} titleIcon={MdEventNote}>
                 <KubePodEvents
                     open
