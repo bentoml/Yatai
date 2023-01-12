c13483904777d05eeff4b2a82ea3ddca229cd945
yetone <yetoneful@gmail.com>
2023-01-12 16:38:24 +0000

    feat(dashboard): deployment JSON editor

--- dashboard/src/schemas/deployment.ts
+++ dashboard/src/schemas/deployment.ts
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-cycle */
-import { ICreateDeploymentTargetSchema } from './deployment_target'
+import { ICreateDeploymentTargetSchema, ICreateDeploymentTargetUISchema } from './deployment_target'
 import { IDeploymentRevisionSchema } from './deployment_revision'
 import { IUserSchema } from './user'
 import { IResourceSchema } from './resource'
@@ -31,8 +31,19 @@ export interface IDeploymentSchema extends IResourceSchema {
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface IDeploymentFullSchema extends IDeploymentSchema {}
 
+export interface IUpdateDeploymentUISchema {
+    targets: ICreateDeploymentTargetUISchema[]
+}
+
 export interface IUpdateDeploymentSchema {
     targets: ICreateDeploymentTargetSchema[]
+}
+
+export interface ICreateDeploymentUISchema extends IUpdateDeploymentUISchema {
+    cluster_name?: string
+    name: string
+    description: string
+    kube_namespace?: string
 }
 
 export interface ICreateDeploymentSchema extends IUpdateDeploymentSchema {
